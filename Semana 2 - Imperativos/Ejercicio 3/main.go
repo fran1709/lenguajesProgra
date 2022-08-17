8e04f0938da2d7b2c85cd2bc9dc365e742308c35
Francisco Ovares Rojas <82686857+fran1709@users.noreply.github.com>
2022-08-16 19:37:39 -0600

    Ejercicio 3 Completo

---
+++ Semana 2 - Imperativos/Ejercicio 3/main.go
@@ -0,0 +1,70 @@
+package main
+
+import (
+	"fmt"
+)
+
+/*
+*
+Rota los elementos una posición hacia la derecha.
+*/
+func dereRotar(pArray *[10]int, size int) {
+	var i int
+	var last int = pArray[size-1]
+	var aux int
+	for i = size - 1; i > 0; i-- {
+		if i-1 < 0 {
+			break
+		}
+		aux = pArray[i-1]
+		pArray[i] = aux
+		//fmt.Println(pArray)
+	}
+	pArray[0] = last
+}
+
+/*
+*
+Rota los elementos una posición hacia la izquierda.
+*/
+func izquiRotar(pArray *[10]int, size int) {
+	var i int
+	var first int = pArray[0]
+	var aux int
+	for i = 0; i < size; i++ {
+		if i+1 == size {
+			break
+		}
+		aux = pArray[i+1]
+		pArray[i] = aux
+		//fmt.Println(pArray)
+	}
+	pArray[size-1] = first
+}
+
+/*
+*
+Rota los elementos n cantidad de veces especificadas respectivamente.
+*/
+func rotacion(pArray *[10]int, size int) {
+	var i int
+	var cantIzq int = 3
+	var cantDer int = 7
+	for i = 0; i < cantIzq; i++ {
+		izquiRotar(pArray, size)
+	}
+	for i = 0; i < cantDer; i++ {
+		dereRotar(pArray, size)
+	}
+
+}
+
+func main() {
+	var arrayOne = [10]int{0, 1, 2, 3, 4, 5, 6, 7, 8, 9}
+	puntero := arrayOne
+
+	rotacion(&puntero, len(arrayOne))
+	fmt.Println(arrayOne)
+	fmt.Println(puntero)
+
+}
