698c9ebbc9c54d1d95c2129b3f628ff649cdb7e1
fran1709 <fran1709@estudiantec.cr>
2022-11-16 00:25:33 -0600

    updt

--- POO/excepciones/JavaVersion/src/App.java
+++
@@ -1,9 +0,0 @@
-import com.Solve;
-
-public class App {
-    public static void main(String[] args) {
-        Solve Calculadora = new Solve();
-        String operacion = "512*30/2+320-1";
-        System.out.println("Resultado -> " + Calculadora.result(operacion)); 
-    }
-}
--- POO/excepciones/JavaVersion/src/exception/Not_Valid.java
+++
@@ -1,7 +0,0 @@
-package exception;
-
-public class Not_Valid extends NumberFormatException{
-    public Not_Valid(String pMsg){
-        super(pMsg);
-    }
-}
